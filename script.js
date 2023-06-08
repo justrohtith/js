24d5daa913d9717a042eaf8291fa918393dcfa6a
Aman <135516605+justrohtith@users.noreply.github.com>
2023-06-09 01:46:23 +0530

    css

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-let laptop = {
-    cpu: 'i9',  
-    ram: '8',   
-    brand: 'HP',    
 
-    getConfig: function(){
-
-        console.log(laptop)
+console.log(func, func3)
+let func = () => {
+    let laptop = {
+        cpu: 'i9',  
+        ram: '8',   
+        brand: 'HP',    
+    
+        getConfig: function(){
+            console.log(this,'func');
+        }
     }
+    laptop.getConfig()
+    console.log(this);
 }
 
-laptop.getConfig();
+var func3 = () => {
+    let laptop = {
+        cpu: 'i9',  
+        ram: '8',   
+        brand: 'HP',    
+    
+        getConfig: function(){
+            console.log(this,'func');
+        }
+    }
+    laptop.getConfig()
+    console.log(this);
+}
+
+
+function func2(){
+    let laptop = {
+        cpu: 'i9',  
+        ram: '8',   
+        brand: 'HP',    
+    
+        getConfig: function(){
+            console.log(this);
+        }
+    }
+    laptop.getConfig()
+    console.log(this);
+    func();
+}
+
+let laptop = {
+    getConfig: function(){
+        this.a = 'aman'
+        console.log(this);
+
+    },
+}
+laptop.getConfig = {}
+
+laptop.getConfig.a = 'sdfg'
+console.log(laptop.getConfig)
+    
+function getConfig() {
+        this.a = 'aman'
+        console.log(this);
+}
+
+const s = new getConfig(a);
